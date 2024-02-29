d35ea120759032f35722b566a725110fd804ed9f
Adwi Arifin <adwiarifin@gmail.com>
2024-02-29 09:33:38 +0700

    add config module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'mysql://root@localhost:3307/nestjs',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
