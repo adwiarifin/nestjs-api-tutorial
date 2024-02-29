11d7d8079f72e5af022ac6120286c8c2a4de7aa7
Adwi Arifin <adwiarifin@gmail.com>
2024-02-29 10:12:44 +0700

    adding passport for signing jwt

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,14 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+}
